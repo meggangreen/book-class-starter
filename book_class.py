ad1d9bceaf3d32a006c8b3867ee2e32c244ab5a3
Meggan M Green <github@meggangreen.com>
2018-02-11 01:16:00 +0000

    book class to count words

---
+++ book_class.py
@@ -0,0 +1,19 @@
+class Book(object):
+
+    def __init__(self, title=None):
+        self.title = title
+        self.word_counts = {}
+
+    def __repr__(self):
+        return ('<Book "{}" | Words counted: {}>'
+                ).format(self.title, str(len(self.word_counts) > 0))
+
+    def fill_word_counts(self, text):  # text as string
+        words = text.split()
+        for word in words:
+            # correct capitalization
+            # correct punctuation
+            self.word_counts[word] = self.word_counts.get(word, 0) + 1
+
+    def get_word_counts(self, word):  # word as string
+        return self.word_counts.get(word, 0)
