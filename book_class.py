7fac5791b6bb31161b4b7eeb52c3fef7b4f7f98b
Meggan M Green <github@meggangreen.com>
2018-04-29 01:07:20 +0000

    line spacing

--- book_class.py
+++ book_class.py
@@ -5,9 +5,11 @@ class Book(object):
         self.word_counts = {}
         # other properties like author, publisher, etc
 
+
     def __repr__(self):
         return ('<Book "{}" | Words counted: {}>'
                 ).format(self.title, str(len(self.word_counts) > 0))
+
 
     def fill_word_counts(self, text):  # text as string
         words = text.split()
@@ -15,6 +17,7 @@ class Book(object):
             # correct capitalization
             # correct punctuation
             self.word_counts[word] = self.word_counts.get(word, 0) + 1
+
 
     def get_word_counts(self, word):  # word as string
         return self.word_counts.get(word, 0)
